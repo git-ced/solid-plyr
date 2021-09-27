8b8e6bb65b076048f48808d196e5fc0a1da93eb2
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2021-09-28 03:49:38 +0800

    feat: create `YoutubeDemo` component

---
+++ example/src/components/YoutubeDemo.tsx
@@ -0,0 +1,56 @@
+// ANCHOR Solid
+import {
+  createSignal,
+  createEffect,
+  JSX,
+} from 'solid-js';
+
+// ANCHOR Plyr
+import { SourceInfo } from 'plyr';
+
+// ANCHOR Solid Plyr
+import {
+  createPlyr,
+  UncontrolledPlyr,
+} from '../../../src';
+
+const youtubeSrc: SourceInfo = {
+  type: 'video',
+  sources: [
+    {
+      src: 'yWtFb9LJs3o',
+      provider: 'youtube'
+    }
+  ]
+};
+
+export default function YoutubeDemo(): JSX.Element {
+  const [plyr, setPlyr] = createPlyr({
+    source: youtubeSrc,
+  });
+  const [loading, setLoading] = createSignal(true);
+
+  createEffect(() => {
+    const youtubePlayer = plyr()?.plyr;
+
+    if (youtubePlayer) {
+      setLoading(false);
+      youtubePlayer.on('timeupdate', event => {
+        const instance = event.detail.plyr;
+        console.log(instance.currentTime);
+      });
+    }
+  })
+
+  return (
+    <div>
+      <h2 class="text-center text-2xl font-medium text-gray-600 mb-6">
+        YouTube Usage
+      </h2>
+      <UncontrolledPlyr
+        ref={setPlyr}
+        isLoading={loading()}
+      />
+    </div>
+  );
+}
