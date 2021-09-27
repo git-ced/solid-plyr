6e17a0078f1eaaf3e80f60561e7194fecf15c551
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2021-09-28 01:05:38 +0800

    feat: create `SolidPlyr` component

---
+++ src/SolidPlyr/index.tsx
@@ -0,0 +1,27 @@
+// ANCHOR Solid
+import { JSX } from 'solid-js';
+
+// ANCHOR Signals
+import createPlyr from '../createPlyr';
+
+// ANCHOR Types
+import {
+  CreatePlyrProps,
+} from '../createPlyr/index.d';
+
+// ANCHOR Components
+import UncontrolledPlyr from '../UncontrolledPlyr';
+import '../styles.css';
+
+export default function SolidPlyr(
+  props: CreatePlyrProps,
+): JSX.Element {
+  const [plyr, setPlyr] = createPlyr(props);
+
+  return (
+    <UncontrolledPlyr
+      ref={setPlyr}
+      isLoading={!plyr()}
+    />
+  );
+}
