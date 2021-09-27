e44ca4faf7e04ccd92cef7a2f6eacf8b5bc70772
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2021-09-28 03:49:16 +0800

    feat: create `HlsDemo` component

---
+++ example/src/components/HlsDemo.tsx
@@ -0,0 +1,57 @@
+// ANCHOR Solid
+import {
+  createSignal,
+  createEffect,
+  JSX,
+} from 'solid-js';
+
+// ANCHOR Plyr
+import { SourceInfo } from 'plyr';
+
+// ANCHOR Solid Plyr
+import {
+  createHlsPlyr,
+  UncontrolledPlyr,
+} from '../../../src';
+
+const hlsSrc: SourceInfo = {
+  type: 'video',
+  sources: [
+    {
+      src:
+        'https://bitmovin-a.akamaihd.net/content/playhouse-vr/m3u8s/105560.m3u8',
+      type: 'application/x-mpegURL'
+    }
+  ]
+};
+
+export default function HlsDemo(): JSX.Element {
+  const [plyr, setPlyr] = createHlsPlyr({
+    source: hlsSrc,
+  });
+  const [loading, setLoading] = createSignal(true);
+
+  createEffect(() => {
+    const hlsPlayer = plyr()?.plyr;
+
+    if (hlsPlayer) {
+      setLoading(false);
+      hlsPlayer.on('timeupdate', event => {
+        const instance = event.detail.plyr;
+        console.log(instance.currentTime);
+      });
+    }
+  })
+
+  return (
+    <div>
+      <h2 class="text-center text-2xl font-medium text-gray-600 mb-6">
+        HLS Usage
+      </h2>
+      <UncontrolledPlyr
+        ref={setPlyr}
+        isLoading={loading()}
+      />
+    </div>
+  );
+}
