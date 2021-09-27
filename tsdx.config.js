26a171785fcb4852de916f88f92299da8d8c7a68
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2021-09-27 22:54:01 +0800

    chore: initialize `solid-plyr` with pridepack

---
+++ tsdx.config.js
@@ -0,0 +1,22 @@
+const postcss = require('rollup-plugin-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+
+module.exports = {
+  rollup(config, options) {
+    config.plugins.push(
+      postcss({
+        plugins: [
+          autoprefixer(),
+          cssnano({
+            preset: 'default',
+          }),
+        ],
+        inject: false,
+        extract: true,
+        extract: 'lion-skin.min.css',
+      })
+    );
+    return config;
+  },
+};
